748cd6113fbf4b8b583ca0a570a367e32b3be62a
Даниил <bronnikovds@gmail.com>
2025-04-17 19:28:55 +0500

    init server and app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', routes);
+module.exports = app;
